96f2ff2140035d933fed540cb6cd750cea57485c
Utkarsh <suryautkarsh1919@gmail.com>
2025-06-24 21:22:36 +0530

    Added email verification and sending email for bookings

--- models/user.js
+++ models/user.js
@@ -5,8 +5,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     isVerified: {
         type: Boolean,
